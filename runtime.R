6878c772f84206d8cfadfb59c6b370c016eb21d5
Michael Burkhardt <mihobu@users.noreply.github.com>
2021-08-31 16:20:05 -0400

    Create runtime.R

---
+++ runtime.R
@@ -0,0 +1,26 @@
+output <- tryCatch(
+  {
+    library(jsonlite)
+    HANDLER <- Sys.getenv("_HANDLER")
+    args <- commandArgs(trailingOnly = TRUE)
+    EVENT_DATA <- paste(args, collapse=' ')
+    HANDLER_split <- strsplit(HANDLER, ".", fixed = TRUE)[[1]]
+    file_name <- paste0(HANDLER_split[1], ".R")
+    function_name <- HANDLER_split[2]
+    source(file_name)
+    params <- fromJSON(EVENT_DATA)
+    output <- tryCatch(
+      list(out = do.call(function_name, params), quit_status = 0),
+      error = function(e) {
+        list(out = e$message, quit_status = 100)
+      }
+    )
+
+    list(out = output$out, quit_status = output$quit_status)
+  },
+  error = function(e) {
+    list(out = e$message, quit_status = 100)
+  }
+)
+output$out
+quit(status = output$quit_status)
